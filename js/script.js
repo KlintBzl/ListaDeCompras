dbe1dd0f6f6bfecb950ba8526692a7236a81ef87
KlintBzl <klintburzlaff@gmail.com>
2025-05-04 18:08:27 -0300

    site em processo

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const frm = document.getElementById("Formula");
+const nome = document.getElementById("NProduto");
+const Qtd = document.getElementById("qtd");
+const botaoAdd = document.getElementById("Add");
+
