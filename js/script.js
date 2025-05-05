1955bbf9ecc34ba9910fc9acd2e6e207adde12b3
KlintBzl <klintscouts@gmail.com>
2025-05-05 17:07:06 -0300

    AAAHHHH

--- js/script.js
+++ js/script.js
@@ -1,7 +1,22 @@
-const frm = document.getElementById("Formula");
-const nome = document.getElementById("NProduto");
-const Qtd = document.getElementById("qtd");
-const botaoAdd = document.getElementById("Add").addEventListener('click', function() {
-    alert('Item adicionado');
-  });;
+const nome = document.getElementById("produto");
+const Qtd = document.getElementById("quantidade");
 
+let listaCompras = [];
+
+function adicionarItem(item) {
+    if (!listaCompras.includes(item)) {
+        listaCompras.push(item);
+        console.log(`${item} adicionado à lista.`);
+    } else {
+        console.log(`${item} já está na lista.`);
+    }
+}
+function mostrarLista() {
+    console.log("Lista de Compras:");
+    listaCompras.forEach((item, index) => {
+        console.log(`${index + 1}. ${item}`);
+    });
+}
+
+adicionarItem(`${nome}`);
+mostrarLista();
